ca4fb4f731954f817997f597e6d20d0376b961a2
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2017-02-28 05:55:54 -0800

    Final version

---
+++ Fonts.NT/FavoritesList.swift
@@ -0,0 +1,36 @@
+//
+//  FavoritesList.swift
+//  Fonts
+//
+//  Created by Nicholas Tran on 2/13/17.
+//  Copyright © 2017 NT Dev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class FavoritesList {
+    static let sharedFavoritesList = FavoritesList()
+    private(set) var favorites:[String]
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavorites = defaults.object(forKey: "favorites") as? [String]
+        favorites = storedFavorites != nil ? storedFavorites! : []
+    }
+    func addFavorite(fontName: String) {
+        if !favorites.contains(fontName) {
+            favorites.append(fontName)
+            saveFavorites()
+        }
+    }
+    func removeFavorite(fontName: String) {
+        if let index = favorites.index(of: fontName) {
+            favorites.remove(at: index)
+            saveFavorites()
+        }
+    }
+    private func saveFavorites() {
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        defaults.synchronize()
+    }
+}
---
+++ Fonts.NT/RootViewController.swift
@@ -0,0 +1,158 @@
+//
+//  RootViewController.swift
+//  Fonts
+//
+//  Created by Nicholas Tran on 2/13/17.
+//  Copyright © 2017 NT Dev. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+
+    private var familyNames: [String]!
+    private var cellPointSize: CGFloat!
+    private var favoritesList: FavoritesList!
+    private static let familyCell = "FamilyName"
+    private static let favoritesCell = "Favorites"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        familyNames = (UIFont.familyNames as [String]).sorted()
+        let preferredTableViewFont =
+            UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        cellPointSize = preferredTableViewFont.pointSize
+        favoritesList = FavoritesList.sharedFavoritesList
+        tableView.estimatedRowHeight = cellPointSize
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    //override func didReceiveMemoryWarning() {
+        //super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    //}
+
+    // MARK: - Table view data source
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont? {
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            let fontName = UIFont.fontNames(forFamilyName: familyName).first
+            return fontName != nil ?
+                UIFont(name: fontName!, size: cellPointSize) : nil
+        } else {
+            return nil
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return favoritesList.favorites.isEmpty ? 1 : 2
+    }
+    
+    //override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        //return section == 0 ? familyNames.count : 1
+    //}
+    
+    func tableView(tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "All Font Families" : "My Favorite Fonts"
+    }
+    
+    func tableView(tableView: UITableView,
+                            cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            // The font names list
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: RootViewController.familyCell,
+                for: indexPath as IndexPath)
+            cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath)
+            cell.textLabel?.text = familyNames[indexPath.row]
+            cell.detailTextLabel?.text = familyNames[indexPath.row]
+            return cell
+        } else {
+            // The favorites list
+            return tableView.dequeueReusableCell(
+                withIdentifier: RootViewController.favoritesCell,
+                for: indexPath as IndexPath)
+        }
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
